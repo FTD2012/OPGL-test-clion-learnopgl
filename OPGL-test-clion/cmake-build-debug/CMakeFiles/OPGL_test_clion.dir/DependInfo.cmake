e54f541bb284c52fd1ee085c21a34a218dde87bc
FTD2012 <962831141@qq.com>
2017-07-16 19:43:55 +0800

    OPGL-test-clion git init

---
+++ OPGL-test-clion/cmake-build-debug/CMakeFiles/OPGL_test_clion.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/invoker/Documents/Workspace/OPGL/OPGL-test-clion/OPGL-test-clion/glad.c" "/Users/invoker/Documents/Workspace/OPGL/OPGL-test-clion/OPGL-test-clion/cmake-build-debug/CMakeFiles/OPGL_test_clion.dir/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "../GLAD"
+  "../GLAD/glad"
+  "/usr/local/Cellar/glfw/3.2.1/include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/invoker/Documents/Workspace/OPGL/OPGL-test-clion/OPGL-test-clion/main.cpp" "/Users/invoker/Documents/Workspace/OPGL/OPGL-test-clion/OPGL-test-clion/cmake-build-debug/CMakeFiles/OPGL_test_clion.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "../GLAD"
+  "../GLAD/glad"
+  "/usr/local/Cellar/glfw/3.2.1/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ OPGL-test-clion/cmake-build-debug/CMakeFiles/OPGL_test_clion.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OPGL_test_clion.dir/main.cpp.o"
+  "CMakeFiles/OPGL_test_clion.dir/glad.c.o"
+  "OPGL_test_clion.pdb"
+  "OPGL_test_clion"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/OPGL_test_clion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

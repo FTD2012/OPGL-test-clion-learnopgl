8f2f214f8572a0c51d13e8a2f5c8da2339be3c66
FTD2012 <962831141@qq.com>
2018-05-09 19:10:03 +0800

    feat: shader 练习2，使用uniform为三角形添加水平偏移

--- OPGL-test-clion/1.3_hello_shader/hello_shader.cpp
+++ OPGL-test-clion/1.3_hello_shader/hello_shader.cpp
@@ -169,10 +169,8 @@ int main() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-//        float timeValue = glfwGetTime();
-//        float greenValue = (sin(timeValue)/2.0f) + 0.5f;
         shaderProgram.use();
-//        shaderProgram.setFloat("vertexColor", greenValue);
+        shaderProgram.setFloat("xOffset", 0.5f);
 
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
--- OPGL-test-clion/shader/hello_shader.vert
+++ OPGL-test-clion/shader/hello_shader.vert
@@ -2,9 +2,10 @@ const char *hello_triangle_vertexShaderSource = R"(#version 330 core
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
 out vec4 vertexColor;
+uniform float xOffset;
 void main()
 {
-    gl_Position = vec4(aPos.x, -aPos.y, aPos.z, 1.0);
+    gl_Position = vec4(aPos.x + xOffset, -aPos.y, aPos.z, 1.0);
     vertexColor = vec4(aColor, 1.0f);
 }
 )";
